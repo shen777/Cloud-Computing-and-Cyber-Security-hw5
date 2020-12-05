343019f9e81391265258dc4e9036080d7b4c0560
shen777 <49338852+shen777@users.noreply.github.com>
2020-12-05 21:33:33 +0800

    no extend filter

---
+++ server.js
@@ -0,0 +1,81 @@
+console.log('May Node be with you')
+const express = require('express');
+const bodyParser= require('body-parser')
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+
+MongoClient.connect('mongodb+srv://yoda:Chosen1@cluster0.dcekc.mongodb.net/Cluster0?retryWrites=true&w=majority', { useUnifiedTopology: true})
+    .then(client => {
+          console.log('Connected to Database')
+          const db = client.db('star-wars-quotes')
+          const quotesCollection = db.collection('quotes')
+          
+          app.use(bodyParser.urlencoded({ extended: true }))
+          app.use(express.static('public'))
+          app.use(bodyParser.json())
+          app.set('view engine', 'ejs')
+          
+          app.listen(3000, function() {
+          console.log('listening on 3000')
+          })
+
+          app.get('/', (req, res) => {
+                  db.collection('quotes').find().toArray()
+                    .then(results => {
+                          console.log(results)
+                          res.render('index.ejs', { quotes: results })
+                    })
+                  .catch(error => console.error(error))
+          // ...
+          })
+          
+          app.delete('/quotes', (req, res) => {
+            quotesCollection.deleteOne(
+              { name: req.body.name }
+            )
+              .then(result => {
+                if (result.deletedCount === 0) {
+                  return res.json('No quote to delete')
+                }
+                res.json('Deleted Darth Vadar\'s quote')
+              })
+              .catch(error => console.error(error))
+          })
+
+          app.put('/quotes', (req, res) => {
+            quotesCollection.findOneAndUpdate(
+              { name: 'Yoda' },
+              {
+                $set: {
+                  name: req.body.name,
+                  quote: req.body.quote
+                }
+              },
+              {
+                upsert: true
+              }
+            )
+              .then(result => res.json('Success'))
+              .catch(error => console.error(error))
+          })
+          
+          
+          app.post('/quotes', (req, res) => {
+              quotesCollection.insertOne(req.body)
+                   .then(result => {
+                         res.redirect('/')
+                         //console.log(result)
+                         })
+                   .catch(error => console.error(error))
+                   })
+
+
+          
+          })
+    .catch(error => console.error(error))
+
+
+
+
+
